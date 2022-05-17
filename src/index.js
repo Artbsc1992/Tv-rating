3171e28998e9ea0390a09c0c6da96229a3e1d9c1
Ronald <rjskinnerc@unal.edu.co>
2022-05-17 17:50:16 -0500

    Fix button listeners

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import '../assets/icons/logo.png';
 import '../assets/icons/fav.png';
 import './style.css';
-import showPopUp from './modules/reservations.js';
 import render from './modules/display.js';
 
 render();
-
-const reservationBtns = document.querySelectorAll('[data-reservation]');
-
-reservationBtns.forEach((reservationBtn) => {
-  reservationBtn.addEventListener('click', (e) => showPopUp(e.target.dataset.id));
-});
