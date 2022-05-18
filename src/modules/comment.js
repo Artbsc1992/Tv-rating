e6deb6ff0620551f498d7cfe871c8cf8b6da1b0d
skj99667788@gmail.com <skj99667788@gmail.com>
2022-05-18 18:57:11 +0500

    comment popup

---
+++ src/modules/comment.js
@@ -0,0 +1,52 @@
+import fetchShow from './reservations-api.js';
+
+const comment = document.createElement('div');
+comment.classList.add('comment');
+
+const closePopUp = () => {
+  document.body.removeChild(comment);
+  comment.innerHTML = '';
+};
+
+const showPop = async (showId) => {
+  try {
+    document.body.append(comment);
+    const show = await fetchShow(showId);
+    comment.innerHTML = ` <div class="img-sec">
+    <img class="movie-img" src="${show.image.original}" alt="${show.name}">
+    <span id="close-reservation-popup"><img src="./img/close.png" alt="Close Reservation Popup"/></span>
+</div>
+<div class="detail-sec" >
+<h2> ${show.name} </h2>
+<div class="movie-details">
+    <ul>
+    <li>${show.summary}</li>
+    <li>Language: ${show.language}</li>
+    <li>Avg. Rating: ${show.rating.average}</li>
+    <li>Show type:  ${show.type}</li>
+    <li>Genres: ${show.genres.map((genre) => `<span class="genre-label"> ${genre}</span>`)}</li>
+    </ul>
+</div>
+</div>
+<div class="comment-sec">
+    <h2>Add a comment</h2>
+    <form action="">
+        <input type="text" placeholder="Your name">
+        <textarea name="" id="" cols="10" rows="10" placeholder="your insights" ></textarea>
+    </form>
+</div>
+    `;
+  } catch (error) {
+    comment.innerHTML = `
+      <div class="reservation-inner">
+        <span id="close-reservation-popup" ><img src="./img/close.png" alt="Close Reservation Popup"/></span>
+        <p>Something went wrong. Please try again later. </p>
+        <small>${error && error}</small>
+      </div>
+    `;
+  }
+
+  comment.querySelector('#close-reservation-popup').addEventListener('click', closePopUp);
+};
+
+export default showPop ;
--- src/modules/display.js
+++ src/modules/display.js
@@ -1,5 +1,6 @@
 import fetchShows from './fetchAPI.js';
 import showPopUp from "./reservations";
+import showPop from "./comment";
 const ul = document.createElement('ul');
 ul.classList = 'showList';
 const div = document.querySelector('.shows');
@@ -12,14 +13,20 @@ const render = async () => {
     li.innerHTML = `
             <img src="${show.img}" alt="logo">
             <p>${show.name} ${show.id}</p>
-            <button type='button'>Comments</button>
+            <button data-comment="true" data-id="${show.id}">Comments</button>
             <button data-reservation="true" data-id="${show.id}">Reservations</button>
         `;
+
 
     const reservationBtn = li.querySelector('[data-reservation]');
 
     ul.append(li);
     reservationBtn.addEventListener('click', (e) => showPopUp(e.target.dataset.id));
+
+    const commentBtn = li.querySelector('[data-comment]');
+
+    ul.append(li);
+    commentBtn.addEventListener('click', (e) => showPop(e.target.dataset.id));
 
   });
 };
